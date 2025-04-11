1612f6e6b3a68a0dc58b24e454d2d6f3fc72c4f1
zazasyach <164740232+zazasyach@users.noreply.github.com>
2025-04-11 18:26:03 +0700

    Add files via upload

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nim' => $this->faker->unique()->randomNumber(8),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->address(),
+            'phone' => $this->faker->phoneNumber(),
+        ];
+    }
+}
+
